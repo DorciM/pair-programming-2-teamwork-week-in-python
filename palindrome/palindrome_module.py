bffb811ca3964c29bae98a004dff503f9559f1a1
DorciM <dora.medgyasszay@gmail.com>
2016-05-30 14:04:17 +0200

    palindrome excercise done

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,17 @@
 def palindrome(str):
-    return
+    str2 = str.lower()
+    str2 = str2.replace(" ", "")
+    str = str.replace(" ", "")
+    if str.lower() == str2[::-1]:
+        #  print "True"
+        return True
+    else:
+        return False
 
 
 def main():
+    sentence = str(input("Please input a string\n"))
+    x = palindrome(sentence)
     return
 
 
