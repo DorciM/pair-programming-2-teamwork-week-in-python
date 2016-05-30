cce35fe8e19ed3176b8382319581f5f7656412a6
DorciM <dora.medgyasszay@gmail.com>
2016-05-30 12:00:31 +0200

    Changed fizzbuzz.py

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,18 @@
 def fizzbuzz(number):
-    return
+    if number %3 == 0:
+        if number %5 == 0:
+            return 'FizzBuzz'
+        else:
+            return 'Fizz'
+    elif number %5 == 0:
+        return 'Buzz'
+    else:
+        return number
 
 
 def main():
+    for i in range (1,101):
+        value = fizzbuzz(i)
     return
 
 if __name__ == '__main__':
