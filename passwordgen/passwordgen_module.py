a4a4dc71b1e607735647ff95d7c06878092bd3db
DorciM <dora.medgyasszay@gmail.com>
2016-05-30 15:03:06 +0200

    magic, it is working

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,32 @@
+import random
+import string
+
+
 def passwordgen():
-    return
+    good_pw = [0,0,0,0]
+    pw = []
+    chars= string.ascii_letters + string.digits + "[!@#$%^&*()?]"
+    for i in range(8):
+        pw.append(random.choice(chars))
+    for i in range(8):
+        if pw[i] in string.ascii_lowercase:
+            good_pw[0] = 1
+        elif pw[i] in string.ascii_uppercase:
+            good_pw[1] = 1
+        elif pw[i] in string.digits:
+            good_pw[2] = 1
+        elif pw[i] in "[!@#$%^&*()?]":
+            good_pw[3] = 1
+    if good_pw == [1, 1, 1, 1]:
+        pw = ''.join(str(i) for i in pw)
+        return pw
+    else:
+        passwordgen()
 
 
 def main():
+    password = passwordgen()
+    print (password)
     return
 
 
