a0cd7aac74425bf883555d9c671737ddeefa79c4
DorciM <dora.medgyasszay@gmail.com>
2016-06-01 09:19:28 +0200

    extras added

--- years/years_module.py
+++ years/years_module.py
@@ -9,8 +9,11 @@ def years(age):
 def main():
     name = str(input("What is your name? \n"))
     age = int(input("How old are you? \n"))
+    number = int(input("Choose a number between 1-10: \n"))
     result = years(age)
-    print("Dear %s you will turn 100 years old in %d" % (name, result))
+    print(("Dear %s you will turn 100 years old in %d") * number % ((name, result) *number))
+    for i in range(number):
+        print("Dear %s you will turn 100 years old in %d" % (name, result))
     return
 
 
