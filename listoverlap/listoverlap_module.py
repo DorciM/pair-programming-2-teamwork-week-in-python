59f45ec0f14108db135e844c822ff4df66545f4a
DorciM <dora.medgyasszay@gmail.com>
2016-06-01 09:29:02 +0200

    added extra 2

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -2,14 +2,13 @@ import random
 
 
 def listoverlap(list1, list2):
-    list3 = []
+    '''list3 = []
     for i in list1:
         if i in list2:
             if i not in list3:
                 list3.append(i)
-    print(list3)
-
-    return list3
+    return list3'''
+    return list(set(list1).intersection(list2))
 
 
 def main():
@@ -17,7 +16,8 @@ def main():
     b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     d = random.sample(range(30), 12)
     e = random.sample(range(30), 12)
-    c = listoverlap(e, d)
+    c = listoverlap(a, b)
+    print(c)
     return
 
 
